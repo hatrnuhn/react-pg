2a5d729314a3da23a1205125d2720dd3c614f819
hatrnuhn <yusron.ibnunaim@gmail.com>
2024-06-06 21:08:47 +0700

    feat: updates UpdateCar page

--- src/pages/UpdateCar.tsx
+++ src/pages/UpdateCar.tsx
@@ -1,6 +1,7 @@
 import { Box, Button, Grid, IconButton, Stack, TextField, Typography } from "@mui/material"
 import AddCircleRoundedIcon from '@mui/icons-material/AddCircleRounded';
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const UpdateCar = () => {
     const Token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZG1pbklkIjoxLCJ1c2VySWQiOjIzLCJlbWFpbCI6ImFkbWluMUBjaDYuY29tIiwidXNlcm5hbWUiOiJhZG1pbjEiLCJpYXQiOjE3MTc1OTcxNDEsImV4cCI6MTcyMDE4OTE0MX0.KoLJ-g7DnnWBsdWHm3p1Yz00S2IUTO5Z8VLyQ6b1KqM'
@@ -16,96 +17,75 @@ const UpdateCar = () => {
         options: [""],
         specs: [""]
     });
+    const [carId, setCarId] = useState('')
 
-    const [options, setOptions] = useState([''])
-    const [specs, setSpecs] = useState([''])
-    const [optionComponents, setOptionComponents] = useState<React.ReactNode[]>([])
-    const [specComponents, setSpecComponents] = useState<React.ReactNode[]>([])
-
+    const handleCarIdChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setCarId(id => id = e.target.value)
+    }
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = e.target
         setUpdate(u => ({...u, [name]: value}))
     }
 
-    const handleOptionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setOptions(os => ([...os, e.target.value]))
+    const handleOptionChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>, index: number) => {
+        const options = [...update.options]
+        options[index] = e.target.value
         setUpdate(u => ({...u, options}))
     }
 
-    const handleSpecChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setSpecs(ss => ([...ss, e.target.value]))
+    const handleSpecChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>, index: number) => {
+        const specs = [...update.specs]
+        specs[index] = e.target.value
         setUpdate(u => ({...u, specs}))
-    }
-
-
-    const OptionComponent = (index: number) => {
-        return (
-            <Stack direction='row' key={index} spacing={1}>
-                <TextField
-                    variant='outlined'
-                    onChange={handleOptionChange}
-                    label='Add an option'
-                />
-                <IconButton 
-                    aria-label="add-another-option" 
-                    color="primary"
-                    onClick={() => handleAdd('option')}
-                >
-                    <AddCircleRoundedIcon />
-                </IconButton>
-            </Stack>
-        )
-    }
-
-    const SpecComponent = (index: number) => {
-        return (
-            <Stack direction='row' key={index} spacing={1}>
-                <TextField
-                    variant='outlined'
-                    onChange={handleSpecChange}
-                    label='Add a specification'
-                />
-                <IconButton 
-                    aria-label="add-another-spec" 
-                    color="primary"
-                    onClick={() => handleAdd('spec')}
-                >
-                    <AddCircleRoundedIcon />
-                </IconButton>
-            </Stack>
-        )
     }
     
     const handleAdd = (component: string) => {
         switch (component) {
             case 'option':
-                setOptionComponents(ocs => {
-                    const index = ocs.length
-                    return ([...ocs, OptionComponent(index)])
-                })
+                setUpdate(u => ({...u, options: [...u.options, '']}))
                 break;
             case 'spec':
-                setSpecComponents(scs => {
-                    const index = scs.length
-                    return ([...scs, SpecComponent(index)])
-                })
+                setUpdate(u => ({...u, specs: [...u.specs, '']}))
                 break;
         }
     }
 
-    const handleSubmit = () => {
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        console.log(update)
+        const payload: Partial<typeof update> = {}
 
+        if (update.description) payload.description = update.description
+        if (update.manufacture) payload.manufacture = update.manufacture
+        if (update.model) payload.model = update.model
+        if (update.options.length) payload.options = update.options
+        if (update.plate) payload.plate = update.plate
+        if (update.rate) payload.rate = update.rate
+        if (update.specs.length) payload.specs = update.specs
+        if (update.transmission) payload.transmission = update.transmission
+        if (update.type) payload.type = update.type
+        if (update.year) payload.year = update.year
+
+        try {
+            const res = await axios.patch(`http://localhost:3000/api/cars/${carId}`, payload, {
+                headers: {
+                    Authorization: `Bearer ${Token}`
+                }
+            })
+            console.log(res.data)
+        } catch (err) {
+            if (err instanceof Error)
+                console.error(err.message)
+        }
     }
 
-
-    useEffect(() => {
-        setOptionComponents([OptionComponent(0)])
-        setSpecComponents([SpecComponent(0)])
-    }, [])
     return (
         <Box component='form' onSubmit={handleSubmit}>
-            <Typography variant='h5'>Values to update</Typography>
-
+            <Stack direction='column' spacing={1}>
+                <Typography variant='h5'>Enter CarId</Typography>
+                <TextField variant='standard' label='CarId' placeholder="Enter carId ..." onChange={handleCarIdChange} />
+                <Typography variant='h5'>Values to update</Typography>
+            </Stack>
             <Grid container spacing={1}>
                 <Grid item xs={6}>
                     <TextField 
@@ -164,26 +144,70 @@ const UpdateCar = () => {
                 <Grid item xs={6}>
                     <TextField 
                         variant='outlined'
+                        label='Type'
+                        name='year'
+                        color='primary'
+                        onChange={handleChange}
+                    />
+                </Grid>
+                <Grid item xs={6}>
+                    <TextField 
+                        variant='outlined'
                         label='Year'
                         name='year'
                         color='primary'
                         onChange={handleChange}
                     />
                 </Grid>
-                <Grid xs={6}/>
                 <Grid item xs={6}>
                     <Typography>Options</Typography>
-                    <Stack direction='row'>
-                        <React.Fragment>{optionComponents.map((oc) => oc)}</React.Fragment>
+                    <Stack direction='column' spacing={0.5}>
+                        {update.options.map((o, index) => {
+                            return (
+                                <Stack direction='row' key={index} spacing={1}>
+                                    <TextField
+                                        variant='outlined'
+                                        onChange={(e) => handleOptionChange(e, index)}
+                                        label='Add an option'
+                                    />
+                                    <IconButton 
+                                        aria-label="add-another-option" 
+                                        color="primary"
+                                        onClick={() => handleAdd('option')}
+                                    >
+                                        <AddCircleRoundedIcon />
+                                    </IconButton>
+                                </Stack>                
+                            )
+                        })}
                     </Stack>
                 </Grid>
                 <Grid item xs={6}>
                     <Typography>Specifications</Typography>
-                    <Stack direction='row'>
-                        <React.Fragment>{specComponents.map((sc) => sc)}</React.Fragment>
+                    <Stack direction='column' spacing={0.5}>
+                        {update.specs.map((s, index) => {
+                            return (
+                                <Stack direction='row' key={index} spacing={1}>
+                                    <TextField
+                                        variant='outlined'
+                                        onChange={(e) => handleSpecChange(e, index)}
+                                        label='Add a specification'
+                                    />
+                                    <IconButton 
+                                        aria-label="add-another-spec" 
+                                        color="primary"
+                                        onClick={() => handleAdd('spec')}
+                                    >
+                                        <AddCircleRoundedIcon />
+                                    </IconButton>
+                                </Stack>
+                            )
+                        })}
                     </Stack>
                 </Grid>
             </Grid>
+
+            <Button variant='contained' color='primary' type='submit' sx={{margin: '1rem'}}>Update Car</Button>
         </Box>
     )
 }
